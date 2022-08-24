892f96ec8767c52b9aecbadb06e4b54e73bd3372
Tedwards <108353463+Tedwards22890@users.noreply.github.com>
2022-08-24 14:45:05 -0400

    updated views.py + created urls within super

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import SuperSerializer
+from .models import Super
+from rest_framework import status
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def super_list(request):
+
+    if request.method == 'GET':
+        supers = super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)    
